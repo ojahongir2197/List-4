28d28f1212509ee595a7ccb3b0bad2a337e0350f
Jahongir <ojahongir2197@gmail.com>
2026-03-16 15:29:53 +0500

    Update and rename README.md to main.py

---
+++ main.py
@@ -0,0 +1,71 @@
+List-4# Tuple va List farqi
+
+# 1
+a = [10, 20, 30]
+a[1] = 50
+print(a)
+
+# 2
+t = (10, 20, 30)
+print(t[1])
+
+# 3
+a = [1, 2, 3]
+print(a)
+
+# 4
+t = (10, 11, 12)
+print(t)
+
+# 5
+a = [3, 6, 9]
+a.append(12)
+print(a)
+
+#Dictionary tushunchasi
+
+# 1
+d = {"ism":"Ali", "yosh":20}
+print(d["ism"])
+
+# 2
+d = {"a":1, "b":2, "c":3}
+print(d.keys())
+
+# 3
+d = {"x":10, "y":20}
+print(d.values())
+
+# 4
+d = {"name":"Tom", "age":25}
+print(d["age"])
+
+# 5
+d = {"a":5, "b":10}
+print(d)
+
+# Dictionary yaratish usullari
+
+# 1
+d = dict(a=1, b=2, c=3)
+print(d)
+
+# 2
+d = {"name":"Ali"}
+d["age"] = 25
+print(d)
+
+# 3
+d = {"a":10, "b":20}
+d["c"] = 30
+print(d)
+
+# 4
+d = {}
+d["name"] = "Aziz"
+print(d)
+
+# 5
+d = {"x":1}
+d["y"] = 2
+print(d)
